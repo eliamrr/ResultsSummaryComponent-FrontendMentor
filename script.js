a79cd295a3a9e546fba58c1e8f16cb6c91dcd3e5
eliamrr <reyeseliam1@gmail.com>
2025-08-04 23:10:23 -0300

    La parte logica terminda

---
+++ script.js
@@ -0,0 +1,36 @@
+async function  cargarDatos() {
+    const respueta = await fetch("data.json")
+    const datos = await respueta.json()
+    const ol = document.getElementById("ol")
+
+    datos.forEach(usuario =>{
+        // Creamos un li
+        const li = document.createElement("li")
+        li.classList.add("lii")
+        li.style.background =  usuario.bg
+        const div = document.createElement("div")
+        div.classList.add("img-p-tw")
+        const img = document.createElement("img")
+        const p1 = document.createElement("p")
+        p1.style.color = `${usuario.color}`
+        p1.classList.add("textt")
+        
+        const p2 = document.createElement("p")
+        p2.classList.add("textt2")
+
+        const espan = document.createElement("span")
+        espan.classList.add("espan")
+
+
+        img.src = usuario.icon
+        p1.textContent = usuario.category
+        
+        espan.textContent = `${usuario.score}`
+        p2.append(espan, " / 100")
+        
+        div.append(img,p1)
+        li.append(div,p2)
+        ol.appendChild(li)
+    })  
+}
+cargarDatos()
